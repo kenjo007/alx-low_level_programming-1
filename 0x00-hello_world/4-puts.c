1a90441dcb4b0749512bd9b4f827793237d9a919
Kemuntofaith <kemuntofaith20@gmail.com>
2022-06-02 04:17:56 -0700

    commit 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,8 @@
 /**
  *main - my entry point 
  *
+ * description - using the puts function for our code
+ * it prints out "Programming is like building a multilingual puzzle"
  *Return - Always o (success)
  */
 int main(void)
