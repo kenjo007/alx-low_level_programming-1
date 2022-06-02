2e8120e4d380a9a541a0d9ab1223dd93fe758c66
Kemuntofaith <kemuntofaith20@gmail.com>
2022-06-02 03:52:17 -0700

    commit 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - my entry point and it has void
+ *
+ *return - should return (0) for well written code
+ */
+int main (void)
+{ 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
