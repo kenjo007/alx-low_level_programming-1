d68f33c90525050632c9df0c62978941960230c5
Kemuntofaith <kemuntofaith20@gmail.com>
2022-06-02 05:21:42 -0700

    commit 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,9 @@
  * main - my entry point
  * description -using the puts function for our code
  * it prints out "prgrammint is like building a multilingual puzzle"
- * Return : Always 0 (success)
- * description : if there is no error in our code, it should return integer 0
+ * Return : Always 0 (success0
+ * description : returning 0 for our code
+ * it gives zero for correct output
  */
 int main(void)
 {
