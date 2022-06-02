dde919b0c47ae5ed470953c6bd8f1745c956e508
Kemuntofaith <kemuntofaith20@gmail.com>
2022-06-02 04:33:39 -0700

    commit 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  * description - using the puts function for our code
  * it prints out "Programming is like building a multilingual puzzle"
  *Return - Always o (success)
- * description - returns 0 for every correct line of codes
+ * description - returns 0 for every correct line of code
  * it returns 0 if there is no error in the output
  */
 int main(void)
