d04193cb49e82519bfb6199529529caa0660215c
Kemuntofaith <kemuntofaith20@gmail.com>
2022-06-02 04:55:05 -0700

    commit 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- *main - my entry point
- * description - using the puts function for our code
- * it prints out "Programming is like building a multilingual puzzle"
- * with the correct output, it returns 0 
- *Return - Always 0 (success)
+ * main - my entry point
+ * description -using the puts function for our code
+ * it prints out "prgrammint is like building a multilingual puzzle"
+ * Return : Always 0 (success)
  */
 int main(void)
 {
