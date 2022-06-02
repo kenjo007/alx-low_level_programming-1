7b661c3925bfc637075c0c31086f8a7f546e839e
Kemuntofaith <kemuntofaith20@gmail.com>
2022-06-02 05:43:34 -0700

    commit 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,8 @@
 
 /**
  * main - my entry point
- * description -using the puts function for our code
- * it prints out "prgrammint is like building a multilingual puzzle"
  * Return : Always 0 (success)
- **/
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
