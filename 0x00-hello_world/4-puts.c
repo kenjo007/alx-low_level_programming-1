58bcebd70407dd62e2f3dc066e66ac53ff1a06d5
Kemuntofaith <kemuntofaith20@gmail.com>
2022-06-02 04:01:27 -0700

    commit 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 /**
+ *4-puts.c is our file
+ *we are using puts instead of printf
+ *in puts we write our line of code
+ */
+
+/**
  *main - my entry point and it has void
  *
  *return - should return (0) for well written code
