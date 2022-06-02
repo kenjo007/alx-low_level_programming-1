795d60d23fe515b40cebe04815ad13e24d1fac45
Kemuntofaith <kemuntofaith20@gmail.com>
2022-06-02 04:10:55 -0700

    commit 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 
 /**
- *4-puts.c is our file
- *we are using puts instead of printf
- *in puts we write our line of code
- */
-
-/**
- *main - my entry point and it has void
+ *main - my entry point 
  *
- *return - should return (0) for well written code
+ *Return - Always o (success)
  */
-int main (void)
+int main(void)
 { 
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
